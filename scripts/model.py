6d4f2fc0349b2395e94d3fafd44d031a5197bdc5
ahunai <tmhrt.hussein.kedir.ali@gmail.com>
2023-12-18 00:00:57 +0300

    engagement done

--- scripts/model.py
+++ scripts/model.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt 
+from matplotlib import style
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, Normalizer
 from sklearn.cluster import KMeans
 from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import MinMaxScaler
 
 class DataModel:
     """
@@ -28,6 +28,47 @@ class DataModel:
         print("Model created Sucessfully.")
         
         return model
+    def normalize(self, x):
+        scaled = StandardScaler().fit_transform(x)
+        return Normalizer().fit_transform(scaled)
+    
+    def cluster(self, x, n, feature_names, cluster_names, elev, azim):
+        model = KMeans(n_clusters = n, init = "k-means++", max_iter = 300, n_init = 10, random_state = 0)
+        y_clusters = model.fit_predict(x)
+
+        colors = ["red", "green", "blue", "yellow", "orange", "purple", "pink", "gray"]
+        fig = plt.figure(figsize = (15,15))
+        ax = fig.add_subplot(111, projection='3d')
+        for i in range(n):
+            ax.scatter(x[y_clusters == i,0],x[y_clusters == i,1],x[y_clusters == i,2], s = 40 , color = colors[i], label = f"cluster {cluster_names[i]}")
+        
+        ax.set_xlabel(f"{feature_names[0]}-->")
+        ax.set_ylabel(f"{feature_names[1]}-->")
+        ax.set_zlabel(f"{feature_names[2]}-->")
+        ax.view_init(elev, azim)
+        ax.legend()
+        plt.show()
+
+        return model.labels_
+
+    def optimum_k(self,X):
+        cost =[]
+        for i in range(1, 11):
+            KM = KMeans(n_clusters = i, max_iter = 500)
+            KM.fit(X)
+            # calculates squared error for the clustered points
+            cost.append(KM.inertia_)     
+        
+        # plot the cost against K values
+        style.use("fivethirtyeight")
+        plt.plot(range(1, 11), cost, color ='g', linewidth ='3')
+        plt.xlabel("Value of K")
+        plt.ylabel("Squared Error (Cost)")
+        plt.show() # clear the plot
+        
+        # the point of the elbow is the 
+        # most optimal value for choosing k
+
 
 
     def clusterGenerator(self, df, selected_features, num_clusters, clust_name):
